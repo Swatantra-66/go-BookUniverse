951e8acee2459c0b883d9c52cd3b1ed4a0169188
Swatantra <maverickswatantra@gmail.com>
2026-01-16 13:17:40 +0530

    feat: Setup API routes and connect them to handlers

---
+++ pkg/routes/routes.go
@@ -0,0 +1,20 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/Swatantra-66/go-bookstore/pkg/handlers"
+	"github.com/gorilla/mux"
+)
+
+// book routing
+func RegisterBookRoutes(router *mux.Router) {
+	router.HandleFunc("/book", handlers.GetBooks).Methods("GET")
+	router.HandleFunc("/book/{bookId}", handlers.GetBookById).Methods("GET")
+	router.HandleFunc("/book", handlers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/{bookId}", handlers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", handlers.DeleteBook).Methods("DELETE")
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	router.PathPrefix("/").Handler(http.StripPrefix("/", fileServer))
+}
