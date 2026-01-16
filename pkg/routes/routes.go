aa9d42da9f21008b939e18e37ad5bd450d8c9798
Swatantra <maverickswatantra@gmail.com>
2026-01-16 17:34:15 +0530

    fix: removed the GetBooks handler

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -9,12 +9,11 @@ import (
 
 // book routing
 func RegisterBookRoutes(router *mux.Router) {
-	router.HandleFunc("/book", handlers.GetBooks).Methods("GET")
+	router.HandleFunc("/book", handlers.GetBookByUser).Methods("GET")
 	router.HandleFunc("/book/{bookId}", handlers.GetBookById).Methods("GET")
 	router.HandleFunc("/book", handlers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/{bookId}", handlers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", handlers.DeleteBook).Methods("DELETE")
-	router.HandleFunc("/book", handlers.GetBookByUser).Methods("GET")
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	router.PathPrefix("/").Handler(http.StripPrefix("/", fileServer))
