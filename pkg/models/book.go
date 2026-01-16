fc92b51b4df31d137f44dbca0fdcc5baa6a1c12c
Swatantra <maverickswatantra@gmail.com>
2026-01-16 13:15:54 +0530

    feat: Define Book struct and GORM migration model

---
+++ pkg/models/book.go
@@ -0,0 +1,79 @@
+package models
+
+import (
+	"github.com/Swatantra-66/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+	IsFav       bool   `json:"isFav"`
+}
+
+func (b *Book) Validate() string {
+	if b.Name == "" {
+		return "Book name cannot be empty"
+	}
+	if b.Author == "" {
+		return "Author name cannot be empty"
+	}
+	if b.Publication == "" {
+		return "Publication cannot be empty"
+	}
+	return "" // Empty string means no error
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookByID(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Find(&book)
+	db.Delete(&book)
+	return book
+}
+
+func UpdateBook(Id int64, book *Book) (*Book, *gorm.DB) {
+	var existingBook Book
+
+	db := db.Where("ID=?", Id).Find(&existingBook)
+
+	if book.Name != "" {
+		existingBook.Name = book.Name
+	}
+	if book.Author != "" {
+		existingBook.Author = book.Author
+	}
+	if book.Publication != "" {
+		existingBook.Publication = book.Publication
+	}
+
+	db.Save(&existingBook)
+	return &existingBook, db
+}
