a820042317aa60bd0e8fb64fc57afb52be8f975f
Swatantra <maverickswatantra@gmail.com>
2026-01-16 14:21:48 +0530

    fix: Enable cloud database connection via env variable

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -8,7 +10,12 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	dsn := "root:user15@tcp(127.0.0.1:3306)/simplerest?charset=utf8&parseTime=True&loc=Local"
+	dsn := os.Getenv("DSN")
+
+	if dsn == "" {
+		dsn = "root:user15@tcp(localhost:3306)/simplerest?charset=utf8&parseTime=True&loc=Local"
+	}
+
 	database, err := gorm.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
