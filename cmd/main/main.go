a0369f02943abda9e41204be5294e72a3c0cecac
Swatantra <maverickswatantra@gmail.com>
2026-01-18 23:23:41 +0530

    feat: added joho for env configration and prevent GEMINI-API-KEY leaking

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,17 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/Swatantra-66/go-bookstore/pkg/config"
 	"github.com/Swatantra-66/go-bookstore/pkg/middleware"
 	"github.com/Swatantra-66/go-bookstore/pkg/routes"
+
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	dir, _ := os.Getwd()
+	fmt.Println("--------------------------------------------------")
+	fmt.Println("📂 SERVER RUNNING FROM:", dir)
+	fmt.Println("--------------------------------------------------")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, assuming environment variables are set.")
+	}
+
+	config.Connect()
 	router := mux.NewRouter()
 	router.Use(middleware.LoggingMiddleware)
 	routes.RegisterBookRoutes(router)
 
+	http.Handle("/", router)
 	fmt.Printf("Server running on http://localhost:8000\n")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
